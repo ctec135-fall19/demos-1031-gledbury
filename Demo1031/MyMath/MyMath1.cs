b796ad00791b16bea14ecebda61c5131a34bb6af
Gledbury <gledbury4@gmail.com>
2019-10-31 15:11:40 -0700

    Thursday1031Demo

---
+++ Demo1031/MyMath/MyMath1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+    public class MyMath1
+    {
+        public static byte Add(byte x, byte y)
+        {
+            // check overflow exception
+            checked
+            {
+                byte b = (byte)(x + y);
+                return b;
+            }
+            //return (byte)(x + y);
+           
+        }
+    }
+}
